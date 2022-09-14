0448ac78c42d29eb1bc0801ff36c33986e332769
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-09-15 01:35:12 +1000

    setup layout and wrap all routes

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,5 +1,21 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const BlogLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
 }
 
 export default BlogLayout
