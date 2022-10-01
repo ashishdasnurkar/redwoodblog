2864da33ed1c640e7d702648d37425ce8b24b244
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-10-01 20:09:59 +1000

    complete contact page form with validation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,19 +1,32 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Form, TextField, TextAreaField, Submit, FieldError, Label} from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 const ContactPage = () => {
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form onSubmit={onSubmit}>
+        <Label name="name" errorClassName="error">Name</Label>
+        <TextField name="name" errorClassName="error" validation={{required: true}}></TextField>
+        <FieldError name="name" className="error"></FieldError>
+
+        <Label name="email" errorClassName="error">Email</Label>
+        <TextField name="email" errorClassName="error" validation={{required: true, pattern: {
+            value: /[^@]+@[^\.]+\..+/,
+          }}}></TextField>
+        <FieldError name="email" className="error"></FieldError>
+
+        <Label name="email" errorClassName="error">Message</Label>
+        <TextAreaField name="message" errorClassName="error" validation={{required: true}}></TextAreaField>
+        <FieldError name="message" className="error"></FieldError>
+
+        <Submit>Send Message</Submit>
+      </Form>
     </>
   )
 }
